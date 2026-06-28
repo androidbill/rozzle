39c2864fcd539e092f34a6ad3b0208c599312074
androidbill <36899036+androidbill@users.noreply.github.com>
2026-06-27 20:56:14 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* The Legend of Rozzle — Service Worker */
 
-const CACHE_NAME = 'legend-of-rozzle-2026.06.27.18';
+const CACHE_NAME = 'legend-of-rozzle-2026.06.27.19';
 const ASSETS = [
   './',
   './index.html',
